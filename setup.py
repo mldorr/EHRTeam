b03b3246c59e18b357557ef7e900d624e3c4cb5f
Hao Fang <hf25@uw.edu>
2018-12-09 16:42:24 -0800

    pylinted

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Sun Nov 25 22:24:24 2018
 
 @author: haofang
 """
 
-from setuptools import setup, find_packages
-from os import path
-
 from io import open
+from os import path
+from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
-long_description="""
+HERE = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+LONG_DESCRIPTION = """
 Public Health Researchers will be using the system to track and study antibiotic resistance patterns and 
 treatment strategies. These users will be very familiar with EHR and eCR. They may have variable levels of 
 familiarity with coding, from completely lacking coding skills to being bioinformatics experts. We would expect 
@@ -25,42 +26,38 @@ these users at minimum to input bacterial strains or antibiotics to the database
 dpf format. This information would be used in public health research to identify and study instances of 
 antibiotic resistance.
 """
+
 setup(
-    name='EHR',  # Required
-    version='1.0.0',  # Required
-    long_description=long_description,  # Optional
-    url='https://github.com/mldorr/EHRTeam',  # Optional
-    author='Maggie Dorr, Hao Fang, Elaine Limqueco, Yiliang Ma, B. Kevin Ramamada',  # Optional
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+    name='EHR',
+    version='1.0.0',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/mldorr/EHRTeam',
+    author='Maggie Dorr, Hao Fang, Elaine Limqueco, Yiliang Ma, B. Kevin Ramamada',
+    classifiers=[
+
         'Development Status :: 3 - Alpha',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Medical',
         'Topic :: Electrical Health Record',
 
-        # Pick your license as you wish
+
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-   # keywords='',  # Optional
-   # Come up with a keywords
 
-    packages=find_packages(),  # Required
-    install_requires=['pandas','numpy','plotly','sqlite3','matplotlib','psycopg2'],  # Optional
-    # add those requaired software that was used in this project, need to add
 
-    package_data={  # Optional
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'plotly', 'sqlite3', 'matplotlib', 'psycopg2',
+                      'PyQt5', 'QtUI_rev', 'fpdf', 'query'],
+
+    package_data={
         'EHRTeam': ['Data/*.*'],
     },
 
 
-    project_urls={  # Optional
+    project_urls={
         'MIMIC III': 'https://mimic.physionet.org/community/contributing/',
         'PHIN VADS': 'https://phinvads.cdc.gov/vads/SearchVocab.action',
         'NARMS': 'https://wwwn.cdc.gov/narmsnow/',
